9a8aca54a04e21884199536a9234c626c3711932
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-08 09:26:30 -0700

    prints using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main (void){
-	char  meg[] ="\"Programming is like building a multilingual puzzle\n";
+	char  meg[] ="\"Programming is like building a multilingual puzzle";
 	puts (meg);
 	return 0;
 }
