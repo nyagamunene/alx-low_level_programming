aa1fa8d78342a9b143632222856536d5f98c8347
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-08 09:59:48 -0700

    Print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - a function that prints using printf
+* Return: 0
+*/
+int main(void)
+{
+	char meg[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf(meg);
+	return (0);
+}
