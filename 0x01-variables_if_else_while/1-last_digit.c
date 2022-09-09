ff5ec72838e8c98cc504f1cfe9f7a8600feaf28d
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-09 11:34:38 -0700

    Checks last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 
 	if (last > 5)
 	{
-		printf("%i is %i and is greater than 5\n", n, last);
+		printf("Last digit of %i is %i and is greater than 5\n", n, last);
 	}
 	else if (last == 0)
 	{
-		printf("%i is 0 and is 0\n", n);
+		printf("Last digit of %i is 0 and is 0\n", n);
 	}
 	else
 	{
-		printf("%i is %i and is less than 6 and not 0\n", n, last);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	}
 	return (0);
 }
