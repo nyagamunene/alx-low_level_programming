d9676e8452e2e33f8887108021f19cbe7cd1519c
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-09 14:05:04 -0700

    Lowercase reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - main block
+*Description: Lowercase reverse
+*Return: 0
+*/
+int main(void)
+{
+	char lower = 'z';
+
+	while (lower >= a)
+	{
+		putchar(lower);
+		lower--;
+	}
+	putchar('\n');
+	return (0);
+}
+
