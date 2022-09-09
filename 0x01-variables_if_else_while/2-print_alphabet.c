80b80197c8ef0e5f28947253e2083db9f60457cb
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-09 12:02:14 -0700

    Alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - main block
+*Description: Prints alphabets in lowercase using putchar
+*Return: 0
+*/
+int main(void)
+{
+	int i = 0;
+	char alphabets[] = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+
+	for (; i < strlen(alphabets; )
+	{
+		putchar(alphabets[i]);
+		putchar("\n");
+		i++;
+	}
+	return (0);
+}
