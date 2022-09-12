3afe0455f9d05cae913f09d31669c2279f0206cf
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-13 02:37:18 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,17 @@
 #include "main.h"
-
+/**
+ * main - print _puchar without standard libraries
+ * Return: 0
+ */
 int main(void)
 {
 	char message[] = "_putchar";
 	int i = 0;
 
-	do
-	{
+	do {
 		_putchar(message[i]);
 		i++;
 	} while (message[i] != '\0');
 	_putchar('\n');
 	return (0);
-
 }
