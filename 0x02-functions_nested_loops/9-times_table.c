4665b2f36d0c4d9f505cda2f33f222dd71ad87ba
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-13 06:03:24 +0300

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - 9 times table
+ * Returns: 0
+ */
+void times_table(void)
+{
+	int increment = 0;
+	int i, j, result;
+
+	for (j = 0; j <= 9; j++)
+	{
+		result = 0;
+		for (i = 0; i <= 9; i++)
+		{
+			if (result < 10)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			result += increment;
+			if (i < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		increment++;
+		_putchar('\n');
+	}
+}
