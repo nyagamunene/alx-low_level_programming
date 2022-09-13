58df1e666b28b64d619aa7ee89239eff1d5e415c
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-13 20:30:31 +0300

    sum of even values in fibonacci sequence upto 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - sum of even values in fibonacci sequence upto 4000000
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int first = 1, second = 1, sum = 0;
+
+	for ( ; second < 4000000; )
+	{
+		if (second % 2 == 0) sum += second;
+		first -= second;
+		second += first;
+		first = second - first;
+
+		second = first + second;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
