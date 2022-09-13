964b71abd8e5ddc8889178ffda610209e56a25cb
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-13 19:23:21 +0300

    Sum of multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - sum of multiples of 3 and 5 between 0 and 1024
+ * Return: 0
+ */
+int main(void)
+{
+	int count, sum = 0, num = 1024;
+
+	for ( count = 0; count < num; count++)
+	{
+		sum += (count % 3 == 0 || count % 5 == 0) ? count : 0;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
