6367810e797092e6ec15cbecbe3da74857cb0b83
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-16 01:57:22 +0300

    Print digits exclude 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "mai.h"
+/**
+*print_most_numbers - print digits except 2 and 4
+*Return: 0
+*/
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	for ( ; a <= 9; a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+	}
+	_putchar('\n');
+}
