16d5926980d39e4ed95c83ddf6db03e286447b5c
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-17 04:37:41 +0300

    Prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * print_number - prints numbers
+ * @n: the number
+ * Return: None
+ */
+void print_number(int n)
+{
+	int rev = 0, rem = 0, neg = 0;
+
+	if (n > 0)
+	{
+		while (n != 0)
+		{
+			rem = n % 10;
+			rev = rev * 10 + rem;
+			n = n / 10;
+		}
+		while (rev != 0)
+		{
+			_putchar(rev % 10 + '0');
+			rev = rev / 10;
+		}
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		neg = -1 * n;
+		_putchar('-');
+		while (neg > 0)
+		{
+			rem = neg % 10;
+			rev = rev * 10 + rem;
+			neg = neg / 10;
+		}
+		while (rev > 0)
+		{
+			_putchar(rev % 10 + '0');
+			rev = rev / 10;
+		}
+	}
+}
