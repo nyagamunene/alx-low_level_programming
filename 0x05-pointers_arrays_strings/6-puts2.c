0b0c242128a374fa7663521a351c848ab78fae60
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-20 01:30:00 +0300

    Puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: The string pointer
+ * Return: None
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	for ( ; str[len] != '\0'; len += 2)
+	{
+		_putchar(str[len]);
+	}
+	_putchar('\n');
+}
