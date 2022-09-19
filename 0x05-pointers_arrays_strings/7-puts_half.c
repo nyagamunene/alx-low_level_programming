c6901467664774bb8c147c1514186107575e7574
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-20 02:21:48 +0300

    Prints second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half the string
+ * @str: The string pointer
+ * Return: None
+ */
+void puts_half(char *str)
+{
+	int len = 0, center, i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+	center = (len % 2 == 0) ? len / 2 : (len - 1) / 2;
+
+	for (i = center + 1; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+}
