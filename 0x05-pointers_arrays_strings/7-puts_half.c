b37f1969f0dcc7fc3f22e92be01d684fb33b2ef8
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-20 02:30:07 +0300

    Prints second half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 	}
 	center = (len % 2 == 0) ? len / 2 : (len - 1) / 2;
 
-	for (i = center; i <= len; i++)
+	for (i = center + 1; i <= len; i++)
 	{
 		_putchar(str[i]);
 	}
