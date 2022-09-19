4c61c8030cdc998e95bb83baf6e2ef98b82c297c
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-20 00:21:50 +0300

    Reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: The string pointer
+ * Return: None
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for ( ; len <= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
