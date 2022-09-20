4fd6db40b02a62b7c9eec01a93eb497ddff2727a
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-20 23:32:54 +0300

    Keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - generate random valid password
+*Return: 0
+*/
+int main(void)
+{
+	srand(time(NULL));
+	int random, i;
+	int range = 126 + 1;
+
+	for (i = 0; i < range; i++)
+	{
+		random = rand() % range + 1;
+		printf("%c", random);
+	}
+	return (0);
+}
