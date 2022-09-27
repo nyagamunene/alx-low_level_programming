c4d890d41b25ff46048c5fe0f1d41e2a9b31bf66
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-27 21:22:36 +0300

    Chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_chessboard - Prints the elements in 8 by 8 matrix.
+*@a: the matrix.
+*Return: None
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for(j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+		j = 0;
+	}
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,10 +3,13 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <unistd.h>
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 
 #endif
