2f0e36e6ed60460ffdfaecfd0ee9a3d3ca4fa9f4
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-28 21:25:57 +0300

    Puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_puts_recursion - prints string
+*@s: The string pointer.
+*Return: None.
+*/
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
+
