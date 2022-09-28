303571ccd058c9aa2d8ea46c79cac0c1f43c41d5
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-29 00:01:10 +0300

    Factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*factorial - calculates the factorial of a number.
+*@n: the number.
+*Return: the factorial
+*/
+int factorial(int n)
+{
+	int prod = 1;
+
+	if (n > 2)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1);
+	}
+}
