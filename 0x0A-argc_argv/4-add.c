def3f6368049cae02ba184766635e9fafd6ff448
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-30 21:39:33 +0300

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,14 +10,14 @@ int main(int argc, char *argv[])
 {
 	int i = 1, add = 0;
 
-	if(argc == 1)
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
 	while (i < argc)
 	{
-		if(atoi(argv[i]) == 0 || atoi(argv[i]) < 0)
+		if (atoi(argv[i]) == 0 || atoi(argv[i]) < 0)
 		{
 			printf("Error\n");
 			return (1);
