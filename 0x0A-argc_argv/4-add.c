fc94e743f037e3f7b32b3aaac64943da364c050b
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-09-30 21:32:53 +0300

    addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - addition.
+*@argc: count of arguments.
+*@argv: arguments.
+*Return: 1 if error and 0 if success
+*/
+int main(int argc, char *argv[])
+{
+	int i = 1, add = 0;
+
+	if(argc == 1)
+	{
+		printf("0");
+		return (0);
+	}
+	while (i < argc)
+	{
+		if(atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		add += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", add);
+	return (0);
+}
