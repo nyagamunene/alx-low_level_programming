f3dae1ba2dadc26a87d0c9680b99e462eafb9005
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-10-05 01:40:55 +0300

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,13 +9,17 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
-	
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	
+
 	array = malloc(size * sizeof(*array));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i <  size; i++)
 	{
 		array[i] = c;
