9fe0c0d60068d41e9af74c81bfe5b899dc3afe8a
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-10-05 23:46:41 +0300

    Free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*free_grid - frees a 2D array.
+*@grid: the grid.
+*@height: the len of the grid.
+*Return: None.
+*/
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+}
