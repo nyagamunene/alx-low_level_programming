ae900eb2ed2ac9e80464d25fbb382ef3cc3756bf
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-10-06 01:31:39 +0300

    split string to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**********************
+	INCOMPLETE
+***********************/
+
+
+
+
+
+/**
+*
+*
+*
+*/
+char **strtow(char *str)
+{
+	char **Array;
+	int len, i, count = 0;
+
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+	for (len = 0; str[len] != "\0"; len++)
+	{
+		if (str[len] == 32)
+		{
+			Array[count] = ;
+		}
+	}
+}
