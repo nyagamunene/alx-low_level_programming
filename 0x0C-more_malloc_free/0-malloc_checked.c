9bda0f311638096426baea64bc4a35c2d6125978
nyagamunene <nyaga.munene2017@students.jkuat.ac.ke>
2022-10-08 00:55:10 +0300

    Malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*malloc_checked - allocates memory using malloc.
+*@b:size of memory.
+*Return: pointer to allocated memory.
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+
+	if (mem == NULL)
+	{
+		exit(98);
+	}
+	return (mem);
+}
